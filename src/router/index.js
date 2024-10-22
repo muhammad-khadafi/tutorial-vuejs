0b644b87963cac9d60c3879b428e795b202bea7b
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2024-10-22 15:00:34 +0700

    Menginstal dan Mengonfigurasi Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/book/Home.vue'
+import CreateBook from '../views/book/CreateBook.vue'
+import EditBook from '../views/book/EditBook.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +9,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/create',
+      name: 'create',
+      component: CreateBook
+    },
+    {
+      path: '/edit',
+      name: 'edit',
+      component: EditBook
     }
   ]
 })
---
+++ src/views/book/Home.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Home Book</h1>
+</template>
